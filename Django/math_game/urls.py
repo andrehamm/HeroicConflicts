92865cc44182e9ff9f547b920c6b576013e35553
Max Harper <mwharp24@gmail.com>
2019-02-18 11:50:50 -0700

    urls for different pages(basic)

--- Django/math_game/urls.py
+++ Django/math_game/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.urls import path
 from django.conf.urls import url, include
 from django.contrib import admin
+from.import views
 
 
 from ski_math import views
 
 urlpatterns = [
         path("", views.home, name="home"),
-        url(r'^admin/',admin.site.urls)
+        url(r'^admin/',admin.site.urls),
+        url(r'^skigame/$', views.skigame),
+        url(r'^portal/$', views.portal),
+        url(r'^stats/$', views.stats),
+        url(r'^$', views.home),
 ]   
--- Django/math_game/views.py
+++ Django/math_game/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.http import HttpResponse
 from django.views import View
-
-# Create your views here.
 
 def home(request):
     return HttpResponse("Ski Math forever!")
 
 def skigame(request):
-    return HttpResponse('Skigame')
+    return HttpResponse('skigame')
 
 def portal(request):
     return HttpResponse('portal')
